200c6711cab13b08078112463ee22bcb787ed281
Anna Pavlova <anna.pbox@gmail.com>
2017-06-25 12:13:57 +0300

    add create-react-app, sass compiler and material-iu

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for material-ui
+injectTapEventPlugin();
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
