e3548a20e18859943f1e4b04712db292a55b6825
Anna Pavlova <a_pavlova@wargaming.net>
2017-07-10 10:17:49 +0300

    add styles for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 import './Header.css'
 
 const Header = () => (
-    <header className='header'>{'AWESOME CINEMA'}</header>
+    <header className='header'>
+        <div className='header-item'>{'Movies'}</div>
+        <div className='header-item'>{'Coming Soon'}</div>
+        <div className='header-item'>{'Location'}</div>
+        <div className='header-item'>{'About'}</div>
+        <div className='header-item-main'>{'Awesome Cinema'}</div>
+    </header>
 )
 
 Header.propTypes = {
